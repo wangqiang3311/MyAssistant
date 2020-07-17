ac96575a3d8b76ff55aa7d62589efb055fb41ce8
wangqiang3311 <wangqiang3311@163.com>
2020-07-17 19:40:12 +0800

    添加dbtest

---
+++ MyAssistant/ColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MyAssistant
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string newColor = "";
+
+            Color c = (Color)System.Windows.Media.ColorConverter.ConvertFromString("SkyBlue");
+
+            var color = value.ToString();
+            if (color == c.ToString())
+            {
+                newColor = "Orange";
+            }
+            return newColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyAssistant/DBTest.cs
@@ -0,0 +1,32 @@
+﻿using ServiceStack;
+using ServiceStack.Configuration;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using System;
+
+namespace MyAssistant
+{
+
+     class DBTest
+     {
+          static void Test()
+          {
+               var appSettings = new AppSettings();
+               var dbTypeSys = appSettings.Get<string>("SYS.DbType");
+               var dbConnectionSys = appSettings.Get<string>("SYS.DbConnection");
+
+               var dbFactory = new OrmLiteConnectionFactory(dbConnectionSys, MySqlDialect.Provider);
+               using var dbFac = dbFactory.OpenDbConnection();
+
+               var groupName = "SLS-30";
+               //var waterStationList3 = dbFac.Select<ViewIotWaterStationDevice>(x => ((x.DeviceClass) == (int)(DeviceClass.WaterTrunkPressureMeter)) && x.GroupName == groupName && (x.Enable == true));
+
+               //foreach (var item in waterStationList3)
+               //{
+               //     Console.WriteLine("linkId:" + item.LinkId);
+               //}
+
+               Console.Read();
+          }
+     }
+}
